2f9e278ca90783c8087b80ff268ee37baf99b23a
krisboz <kristijan.bozinovic1@gmail.com>
2023-04-19 21:38:18 +0200

    Basic frontend

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+const Footer = () => {
+  const footerStyle = {
+    color: "green",
+    fontStyle: "italic",
+    fontSize: 16,
+  };
+
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note App, Kristijan Bozinovic, 2023, Germany</em>
+    </div>
+  );
+};
+
+export default Footer;
